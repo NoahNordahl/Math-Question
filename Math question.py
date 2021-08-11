2afe8ec31f38c7957145b61ddb09e74c2d030340
NoahNordahl <88724989+NoahNordahl@users.noreply.github.com>
2021-08-11 11:56:03 +0200

    Add files via upload

---
+++ Math question.py
@@ -0,0 +1,7 @@
+Math_question = "69 + 420? " # the question
+Question = int(input("What is " + Math_question )) # the questionING
+Answer = int(489) # the correct answer
+if Question == Answer: # basically tells the computer what to do if the SUBJECT answers right
+     print("Congratulations, you won!") # congratualtes you
+else:
+    print("You lose!") # makes you feel bad
